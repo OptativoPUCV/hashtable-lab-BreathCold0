0c3140c158a79eb6638262b556dc2c913b6ba865
repl.it user <replituser@example.com>
2020-09-26 21:20:55 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,6 +82,10 @@ map->size -= 1;
 void * searchMap(HashMap * map,  char * key) {   
 long x = hash(key, map->capacity);
 map->current = x;
+//if (map->buckets[x]->key != key){
+  do {
+  x++;
+}while (map->buckets !=NULL && map->buckets[x]->key != key);
     return map->buckets[x]->value;
 }
 
