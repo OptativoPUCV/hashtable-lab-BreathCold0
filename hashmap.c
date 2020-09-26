3c4c89931ee7e8eee9fb199ea10dfc9aca70fc8f
repl.it user <replituser@example.com>
2020-09-26 21:22:27 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -82,12 +82,13 @@ map->size -= 1;
 void * searchMap(HashMap * map,  char * key) {   
 long x = hash(key, map->capacity);
 map->current = x;
-//if (map->buckets[x]->key != key){
+if (map->buckets[x]->key != key){
   do {
   x++;
 }while (map->buckets !=NULL && map->buckets[x]->key != key);
-    return map->buckets[x]->value;
 }
+return map->buckets[x]->value;
+} //<-- ese creo que faltaba
 
 void * firstMap(HashMap * map) {
 
