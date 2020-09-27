0287d73a8b191c4e88a546c314a23aa492d42341
repl.it user <replituser@example.com>
2020-09-27 19:52:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -114,9 +114,7 @@ if (is_equal(key, map->buckets[x]->key) != 1) {
 }
 
 void * firstMap(HashMap * map) {
-if (map->current <= 0) {
-  map->current = 0;
-}
+
     return map->buckets[map->current]->value;
 }
 
