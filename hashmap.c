22fc90fcd51ea1e2c91ab8aaf0f580a1f20e744d
repl.it user <replituser@example.com>
2020-09-27 19:55:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -114,15 +114,19 @@ if (is_equal(key, map->buckets[x]->key) != 1) {
 }
 
 void * firstMap(HashMap * map) {
-
+if (map->buckets[map->current] == NULL) {
+    do {
+      map->current++;
+    }while (map->buckets[map->current] ==  NULL);
+}
     return map->buckets[map->current]->value;
 }
 
 void * nextMap(HashMap * map) {
-  if (map->buckets[map->current++] != NULL) {
+  if (map->buckets[map->current++] == NULL) {
     do {
       map->current++;
-    }while (map->buckets[map->current++] !=  NULL);
+    }while (map->buckets[map->current++] ==  NULL);
 }
 return map->buckets[map->current++]->key;
 }
