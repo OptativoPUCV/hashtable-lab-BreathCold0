1b84495a15e29ad7358c10f0f000001354fac48e
repl.it user <replituser@example.com>
2020-09-27 19:37:18 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,7 +115,12 @@ if (is_equal(key, map->buckets[x]->key) != 1) {
 
 void * firstMap(HashMap * map) {
 map->current += 1;
+if (map->buckets[map->current] != NULL) {
     return map->buckets[map->current]->value;
+}
+else {
+  return NULL;
+}
 }
 
 void * nextMap(HashMap * map) {
